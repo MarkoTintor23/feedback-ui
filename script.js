9dbcbab04016d2285afbc7ceeaae501e76d2d4c1
Marko Tintor <marko.tintor.18@singimail.rs>
2025-02-10 14:05:35 +0100

    added classes

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-const smileys = document.querySelectorAll(".rating");
-const sendBtn = document.querySelector(".send-review");
-const thanks = document.querySelector("#thank-you");
-const container = document.querySelector(".container");
-const feedback = document.querySelector("#selected-feedback");
+class Feedback {
+  constructor() {
+    this.smileys = document.querySelectorAll(".rating");
+    this.sendBtn = document.querySelector(".send-review");
+    this.thanks = document.querySelector("#thank-you");
+    this.container = document.querySelector(".container");
+    this.feedback = document.querySelector("#selected-feedback");
 
-smileys.forEach(function (smiley) {
-  smiley.addEventListener("click", function () {
-    smileys.forEach(function (s) {
-      s.classList.remove("active");
-    });
-    this.classList.add("active");
-  });
-});
-
-sendBtn.addEventListener("click", function () {
-  const activeSmiley = document.querySelector(".rating.active");
-  if (activeSmiley) {
-    thanks.classList.remove("hidden");
-    container.classList.add("hidden");
-
-    const selectedFeedback = activeSmiley.dataset.feedback;
-    feedback.textContent = `Feedback: ${selectedFeedback}`;
-  } else {
-    alert("You have to pick something");
+    this.addEventListeners();
   }
-});
+
+  addEventListeners() {
+    this.smileys.forEach((smiley) => {
+      smiley.addEventListener("click", (e) => this.smileyHandler(e));
+    });
+    this.sendBtn.addEventListener("click", () => this.sendHandler());
+  }
+
+  smileyHandler(event) {
+    this.smileys.forEach((s) => s.classList.remove("active"));
+    event.target.classList.add("active");
+  }
+
+  sendHandler() {
+    const activeSmiley = document.querySelector(".rating.active");
+    if (activeSmiley) {
+      this.thanks.classList.remove("hidden");
+      this.container.classList.add("hidden");
+
+      const selectedFeedback = activeSmiley.dataset.feedback;
+      this.feedback.textContent = `Feedback: ${selectedFeedback}`;
+    } else {
+      alert("You have to pick something");
+    }
+  }
+}
+
+const smajli = new Feedback();
